3cbffbfc7167f8db198f74b5f3ac7784ea6a1780
harini1810 <137413662+harini1810@users.noreply.github.com>
2025-02-13 21:01:21 +0530

    Update Timestamp.java

--- Timestamp.java
+++ Timestamp.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class TimestampPrinter {
+public class Timestamp {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
